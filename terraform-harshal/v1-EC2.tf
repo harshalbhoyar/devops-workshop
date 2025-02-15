c2c7ce6820867f2dc5c3d31db10217343dd57cbf
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2025-02-15 13:32:59 +0530

    Modified V1-EC2.tf

--- terraform-harshal/v1-EC2.tf
+++ terraform-harshal/v1-EC2.tf
@@ -1,11 +1,72 @@
 # Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
+
+
 provider "aws" {
+  # Specifies the desired AWS region for resource provisioning
   region = "us-east-1"
 }
 
-# Create a resource
+
 resource "aws_instance" "demo-server" {
-    ami           = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
-    key_name = dpp
+  ami             = "ami-04b4f1a9cf54c11d0"
+  instance_type   = "t2.micro"
+  key_name        = "dpp"
+  security_groups = [aws_security_group.terraform-security-group.name]
+  tags = {
+    Name = "terraform-ec2"
+  }
 }
+
+
+resource "aws_security_group" "terraform-security-group" {
+  name = "terraform-security-group"
+
+  #Incoming traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #replace it with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Incoming traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #replace it with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-security-group"
+  }
+
+}
+
+
+
