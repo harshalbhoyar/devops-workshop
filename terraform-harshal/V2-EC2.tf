f7eff6dff524714f112fd5177a050af82998cf38
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2025-02-17 00:07:17 +0530

    Added code for creating multiple instances

--- terraform-harshal/V2-EC2.tf
+++ terraform-harshal/V2-EC2.tf
@@ -14,20 +14,23 @@ provider "aws" {
 
 
 resource "aws_instance" "demo-server" {
-  ami             = "ami-04b4f1a9cf54c11d0"
-  instance_type   = "t2.micro"
-  key_name        = "dpp"
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  key_name               = "dpp"
   vpc_security_group_ids = [aws_security_group.demo-sg.id]
-  subnet_id = aws_subnet.dpp-public-subnet-01.id
-#   security_groups = [aws_security_group.terraform-security-group.name]
+  subnet_id              = aws_subnet.dpp-public-subnet-01.id
+  #   security_groups = [aws_security_group.terraform-security-group.name]
+  for_each = toset(["jenkins-master", "build-slave", "ansible"])
   tags = {
-    Name = "terraform-ec2"
+    Name = "${each.key}"
   }
 }
 
 
 resource "aws_security_group" "demo-sg" {
-  name = "demo-sg"
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id      = aws_vpc.dpp-vpc.id
 
   #Incoming traffic
   ingress {
@@ -62,7 +65,7 @@ resource "aws_security_group" "demo-sg" {
   }
 
   tags = {
-    Name = "terraform-security-group"
+    Name = "ssh-prot"
   }
 
 }
@@ -72,46 +75,46 @@ resource "aws_vpc" "dpp-vpc" {
   tags = {
     Name = "dpp-vpc"
   }
-  
+
 }
 resource "aws_subnet" "dpp-public-subnet-01" {
-  vpc_id = aws_vpc.dpp-vpc.id
-  cidr_block = "10.1.1.0/24"
+  vpc_id                  = aws_vpc.dpp-vpc.id
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
   tags = {
     Name = "dpp-public-subent-01"
   }
 }
 resource "aws_subnet" "dpp-public-subnet-02" {
-  vpc_id = aws_vpc.dpp-vpc.id
-  cidr_block = "10.1.2.0/24"
+  vpc_id                  = aws_vpc.dpp-vpc.id
+  cidr_block              = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1b"
+  availability_zone       = "us-east-1b"
   tags = {
     Name = "dpp-public-subent-02"
   }
 }
 resource "aws_internet_gateway" "dpp-igw" {
-  vpc_id = aws_vpc.dpp-vpc.id 
+  vpc_id = aws_vpc.dpp-vpc.id
   tags = {
     Name = "dpp-igw"
-  } 
+  }
 }
 resource "aws_route_table" "dpp-public-rt" {
-  vpc_id = aws_vpc.dpp-vpc.id 
+  vpc_id = aws_vpc.dpp-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dpp-igw.id 
+    gateway_id = aws_internet_gateway.dpp-igw.id
   }
 }
 resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
-  subnet_id = aws_subnet.dpp-public-subnet-01.id
-  route_table_id = aws_route_table.dpp-public-rt.id   
+  subnet_id      = aws_subnet.dpp-public-subnet-01.id
+  route_table_id = aws_route_table.dpp-public-rt.id
 }
 resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
-  subnet_id = aws_subnet.dpp-public-subnet-02.id 
-  route_table_id = aws_route_table.dpp-public-rt.id   
+  subnet_id      = aws_subnet.dpp-public-subnet-02.id
+  route_table_id = aws_route_table.dpp-public-rt.id
 }
 
 
